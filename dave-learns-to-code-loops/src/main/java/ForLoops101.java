de5cce58b64929e894a57be35479fc645c5decff
Amy D'Entremont <adentremont@hubspot.com>
2020-03-22 13:24:45 -0400

    remove accidental push of for loops stuff

--- dave-learns-to-code-loops/src/main/java/ForLoops101.java
+++ dave-learns-to-code-loops/src/main/java/ForLoops101.java
@@ -1,10 +1,3 @@
 public class ForLoops101 {
 
-  // this is an array, it's a simple "data structure"
-  // an array is basically a list
-  // Integers are just whole numbers
-  public static final int[] ARRAY_OF_INTEGERS = new int[]{1, 2, 3, 4, 5};
-
-  // Make a main method
-  // try printing values from the array
 }
